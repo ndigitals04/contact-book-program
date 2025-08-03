31625e04368ed19cef30b65486add2e0ba49bd82
ndigitals04 <ndigitals001@gmail.com>
2025-08-03 16:30:45 +0100

    Modified contact class to run for a contacts dictionary

--- contact_class.py
+++ contact_class.py
@@ -12,26 +12,26 @@ class Contact:
             return ("error", "Invalid phone number format.")
         if name in contact_names:
             return ("error","Contact with this name already exists.")
-        contacts.append({name: {"phone": phone, "email": email, "address": address}})
+        contacts[name] = {"phone": phone, "email": email, "address": address}
         return ("success", f"Contact {name} created successfully.")
 
     def view(self,name_or_phone):
         search_results = []
-        for contact in contacts:
-            for name,value in contact.items():
-                if name_or_phone.isdigit():
-                    if name_or_phone in value["phone"]:
-                        search_results.append([name, value["phone"], value.get("email", "N/A"), value.get("address", "N/A")])
-                else:
-                    if name_or_phone in name:
-                        search_results.append([name, value["phone"], value.get("email", "N/A"), value.get("address", "N/A")])
+        for name,value in contacts.items():
+            if name_or_phone.isdigit():
+                if name_or_phone in value["phone"]:
+                    search_results.append([name, value["phone"], value.get("email", "N/A"), value.get("address", "N/A")])
+            else:
+                if name_or_phone in name:
+                    search_results.append([name, value["phone"], value.get("email", "N/A"), value.get("address", "N/A")])
         
         return ("success", search_results) if search_results else ("not found", "No contacts found.")
 
 
     def delete(self, name):
-        for contact in contacts:
-            contact_to_delete = contact.get(name)
+        contacts.pop(name)
+        return ("success", f"Contact {name} deleted successfully.")
+
 
 
     
