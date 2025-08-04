28a6390f29947ffa9049bc6c5bd4322ccede6a71
ndigitals04 <ndigitals001@gmail.com>
2025-08-04 01:12:09 +0100

    Created function to retrieve contacts from .txt file

---
+++ contacts_app.py
@@ -0,0 +1,19 @@
+from .contact_class import Contact
+from pathlib import Path
+import ast
+
+contacts = readContactsFile()
+test = Contact("Peter Ndukwe",)
+
+def readContactsFile():
+    file = Path("C:/Users/NEW USER/Documents/python/contact-book/contacts.txt")
+    if file.exists():
+        with open(file,"r") as file:
+            file_text = file.read()
+            return ast.literal_eval(file_text)
+    else:
+        with open(file, "w") as contacts_file:
+            contacts_file.write("{}")
+        with open(file, "r") as contacts_file:
+            file_text = contacts_file.read()
+            return ast.literal_eval(file_text)
